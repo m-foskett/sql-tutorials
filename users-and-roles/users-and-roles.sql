82eeacd03363459eb56937e5787c3d5b705eefcb
Mark Foskett <markryanfoskett@gmail.com>
2023-05-28 20:49:03 +1000

    added solution to users and roles

---
+++ users-and-roles/users-and-roles.sql
@@ -0,0 +1,8 @@
+-- Modify only this SQL create table statement and nothing else
+CREATE TABLE usersRoles (
+  userId INTEGER NOT NULL,
+  roleId INTEGER NOT NULL,
+  FOREIGN KEY(userId) REFERENCES users(id),
+  FOREIGN KEY(roleId) REFERENCES roles(id),
+  UNIQUE(userId,roleId)
+);
